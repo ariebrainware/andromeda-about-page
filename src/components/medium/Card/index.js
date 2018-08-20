4f25817b11519cfa5a6c310d242d14be3fcfdd6b
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-20 14:41:17 +0700

    Refactor code

--- src/components/medium/Card/index.js
+++ src/components/medium/Card/index.js
@@ -3,74 +3,25 @@ import { Card, Image } from 'semantic-ui-react'
 
 import ModalDimmer from '../ModalDimmer'
 
+import 'animate.css'
 import './index.css'
 
-const CardProfile = ({ name }) => {
-  if (name === 'Arie') {
+const CardProfile = ({ data, title }) => {
     return (
-      <Card>
-        <Image src='https://avatars0.githubusercontent.com/u/26453961?v=4' />
-        <Card.Content>
-          <Card.Header>Arie Brainware</Card.Header>
-          <Card.Meta>
-            <span className='date'>Full-stack Developer</span>
-          </Card.Meta>
-          <Card.Description>Arie Brainware is a Full-stack Junior Web Developer.</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <ModalDimmer name='ariebrainware'/>
-        </Card.Content>
-      </Card>
+        <Card>
+            <Image src={data.pictureURL} />
+            <Card.Content>
+                <Card.Header>{data.fullname} {title}</Card.Header>
+                <Card.Meta>
+                    <span className='date'>{data.title}</span>
+                </Card.Meta>
+                <Card.Description>{data.description}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <ModalDimmer data={data}/>
+            </Card.Content>
+        </Card>
     )
-  } else if (name === 'Indro') {
-    return (
-      <Card>
-        <Image src='https://avatars0.githubusercontent.com/u/41278410?v=4' />
-        <Card.Content>
-          <Card.Header>Indro Lie</Card.Header>
-          <Card.Meta>
-            <span className='date'>Front-end Developer</span>
-          </Card.Meta>
-          <Card.Description>Indro Lie is a Front-end Junior Web Developer.</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-        <ModalDimmer name='indrolie'/>
-        </Card.Content>
-      </Card>
-    )
-  } else if (name === 'Abba') {
-    return (
-      <Card>
-        <Image src='https://avatars0.githubusercontent.com/u/37859571?v=4' />
-        <Card.Content>
-          <Card.Header>Abba Yosua Siagian</Card.Header>
-          <Card.Meta>
-            <span className='date'>Front-end Developer</span>
-          </Card.Meta>
-          <Card.Description>Abba is a Front-end Junior Web Developer.</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <ModalDimmer name='abbayosua' />
-        </Card.Content>
-      </Card>
-    )
-  } else if (name === 'Wondo') {
-    return (
-      <Card>
-        <Image src='https://avatars1.githubusercontent.com/u/31862158?v=4' />
-        <Card.Content>
-          <Card.Header>Wondo Indra</Card.Header>
-          <Card.Meta>
-            <span className='date'>Back-end Developer</span>
-          </Card.Meta>
-          <Card.Description>Wondo is a Back-end Junior Web Developer.</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <ModalDimmer name='wondoindra' />
-        </Card.Content>
-      </Card>
-    )
-  }
 }
 
 export default CardProfile
