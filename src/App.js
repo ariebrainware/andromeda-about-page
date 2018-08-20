726fe3a915d3e7d8839a51c59375eb9922286aaa
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-20 09:16:48 +0700

    Add Segment Component for each card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-import ContainerParent from './components/large/container'
-import CardProfile from './components/medium/card'
 import { Grid, Segment } from 'semantic-ui-react'
+
+import ContainerParent from './components/large/container'
+import CardProfile from './components/medium/Card'
+
 import 'animate.css'
+import './App.css';
 
 class App extends Component {
   render() {
@@ -11,22 +13,22 @@ class App extends Component {
       <div className="App">
         <ContainerParent>
           <div className='wrapper'>
-          <Grid columns='equal'>
-            <Grid.Row className='animated pulse'>
-              <Grid.Column>
-                <Segment><CardProfile name='Arie' /></Segment>
-              </Grid.Column>
-              <Grid.Column>
-                <Segment> <CardProfile name='Indro' /></Segment>
-              </Grid.Column>
-              <Grid.Column>
-                <Segment> <CardProfile name='Abba' /></Segment>
-              </Grid.Column>
-              <Grid.Column>
-                <Segment><CardProfile name='Wondo' /></Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+            <Grid columns='equal'>
+              <Grid.Row className='animated pulse'>
+                <Grid.Column>
+                  <Segment raised><CardProfile name='Arie' /></Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment raised> <CardProfile name='Indro' /></Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment raised> <CardProfile name='Abba' /></Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment raised><CardProfile name='Wondo' /></Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
 
           </div>
         </ContainerParent>
